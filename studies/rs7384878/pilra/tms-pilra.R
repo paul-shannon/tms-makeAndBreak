ea897dd238a1b954670fcdb8118e0b7f415fe37c
paul-shannon <paul.thurmond.shannon@gmail.com>
2022-06-14 07:55:49 -0700

    first full run

--- studies/rs7384878/pilra/tms-pilra.R
+++ studies/rs7384878/pilra/tms-pilra.R
@@ -1,31 +1,19 @@
-source("tmsMB-class.R")
-#----------------------------------------------------------------------------------------------------
-runTests <- function()
-{
-   test_ctor()
-   test_setStudyRegion()
-   test_tissue()
-   test_run.tms()
-   test_run.trena()
-   test_run.small()
-   test_run.large()
-
-} # runTests
-#----------------------------------------------------------------------------------------------------
-targetGene <- "TFAM"
-trenaProject <- TrenaProjectAD()
 library(TrenaProjectAD)
-source("~/github/tms-makeAndBreak/R/tmsMB-class.R")
+trenaProject <- TrenaProjectAD()
+f <- "~/github/tms-makeAndBreak/R/tmsMB-class.R"
+stopifnot(file.exists(f))
+source(f)
 data.dir <- "../shared"
+stopifnot(file.exists(data.dir))
 targetGene <- "PILRA"
 trenaProject <- TrenaProjectAD()
 
 if(!exists("tbl.fimo")){
    tbl.fimo <- get(load(file.path(data.dir, "tbl.fimo.PILRA.RData")))
    tbl.ampad.eqtls.raw <- get(load(file.path(data.dir, "ampad.eqtls.pilra.plusMinus.1M.RData")))
-   tbl.ampad.eqtls <- subset(tbl.ampad.eqtls.raw, study=="ampad-rosmap" & pvalue < 0.05)
+   tbl.ampad.eqtls <- subset(tbl.ampad.eqtls.raw, study=="ampad-rosmap" & pvalue < 0.001)
    tbl.gtex.eqtls.raw <- get(load(file.path(data.dir, "gtex-eqtls-tbl.RData")))
-   tbl.gtex.eqtls <- subset(tbl.gtex.eqtls.raw, gene==targetGene & pvalue < 0.05)
+   tbl.gtex.eqtls <- subset(tbl.gtex.eqtls.raw, gene==targetGene & pvalue < 0.001)
    data.dir <- "~/github/TrenaProjectAD/inst/extdata/genomicRegions"
    filename <- "mayoAllPeaks.merged.96064x4.RData"
    tbl.mayoAtac <- get(load(file.path(data.dir, filename)))
@@ -59,4 +47,12 @@ tf.candidates <- unique(tbl.tms.filtered$tf)
 tms$run.trena(tf.candidates)
 tbl.trena <- tms$get.trenaTable()
 head(tbl.trena, n=20)
+
+tms$breakMotifs(tbl.trena[1:10,], tbl.tms)
+breaks <- tms$get.motifBreaks()
+tbl.breaks <- tms$get.breaksTable()
+
+filename <- sprintf("%s-results.RData", targetGene)
+save(tbl.trena, tbl.tms, tbl.tms.filtered, breaks, tbl.breaks, file=filename)
+
 
